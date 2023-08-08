0bb4f344387d3ee45961e4895adc0bbb95cad525
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-08 09:25:15 +0000

    [update]3-15bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  post 'books' => 'books#create'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
