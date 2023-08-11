02921f0ab50a753fdf0d0834d0c59537f7914c0b
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-11 05:14:50 +0000

    [update]3-15createが失敗したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,16 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+ 
   def create
     @book = Book.new(book_params)
-    @book.save
+    if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end  
   end
 
   def index
