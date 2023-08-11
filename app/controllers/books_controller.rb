ed7db391c7a12315c22fa83df31ff99b9ca181c4
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-11 04:00:59 +0000

    [update]3-15createが成功したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,12 @@
 class BooksController < ApplicationController
   def new
-     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @book = Book.new
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     @book.save
-    # 4. show画面任意のページにへリダイレクト
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
   end
 
